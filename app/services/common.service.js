cf02c26e88213fdb7deeacd164941912b7f4dc80
ChilliesVo <97328523+chillivo@users.noreply.github.com>
2025-08-22 12:06:05 +0700

    create certificate-ssl-local

--- app/services/common.service.js
+++ app/services/common.service.js
@@ -1,14 +1,18 @@
 import nodemailer from "nodemailer";
+import { appLog } from "../config/winston";
 
 export async function send(from, to, subject, content, configs) {
   const configOptions = {
     host: configs.host ?? "localhost",
     port: configs.port ?? 25,
     secure: configs.secure ?? false,
+    tls: { rejectUnauthorized: false },
   };
+
   if (configs.auth) configOptions.tls = configs.auth;
   if (configs.tls) configOptions.tls = configs.tls;
 
+  appLog.info(`[HARAKA] [TRANSPORT-CONFIG] ${JSON.stringify(configOptions)}`);
   const transporter = nodemailer.createTransport(configOptions);
   const info = await transporter.sendMail({ from, to, subject, text: content });
   return info;
